a63bde3bd8ece00c3c4422402b316b4930956cd9
Heloisa <jauheloisa@gmail.com>
2022-05-09 18:54:00 -0300

    menu responsivo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,39 @@
+import React, { useState } from "react";
+
 function Header() {
+
+    const [isActive, setActive] = useState(true);
+    
+    const handleToggle = () => {
+        setActive(!isActive);
+        console.log(isActive);
+
+        if ( isActive === true ) {
+            document.body.classList.add('active');
+        } else {
+            document.body.classList.remove('active');
+        }
+
+    };
 
     return (
         <nav>
-          <h1>Heloisa Fernanda</h1>
-          <ul>
-            <li>Quem sou</li>
-            <li>Skills</li>
-            <li>Serviços</li>
-            <li>Projetos</li>
-            <li>Contato</li>
-          </ul>
+            <h1>Heloisa Fernanda</h1>
+            <ul className={`${isActive ? "" : "active"}`}>
+                <li>Quem sou</li>
+                <li>Skills</li>
+                <li>Serviços</li>
+                <li>Projetos</li>
+                <li>Contato</li>
+            </ul>
+
+            <div className="mobile" onClick={handleToggle}>
+                <div className="btn-menu-bg" title="Menu">
+                    <button className={`btn-menu ${isActive ? "" : "active"}`}>
+                        <div className="hamburger"></div>
+                    </button>
+                </div>
+            </div>
         </nav>
     );
 }
