05ed21bd9bd5ab762cb1081bc61f4fa163b5b445
Heloisa <jauheloisa@gmail.com>
2022-05-30 11:06:32 -0300

    ajustes reponsivo

--- src/components/main/Welcome.js
+++ src/components/main/Welcome.js
@@ -5,13 +5,12 @@ function Welcome() {
     return (
         <section id="welcome">
             <article>
-                <h1>Olá, eu sou a <br /> Heloisa :)</h1>
+                <h1>Olá, eu sou a Heloisa :)</h1>
                 <p>Desenvolvedora Front-End & UI Designer.</p>
                 <div className="btns_welcome">
                     <a href="#contact">Contato</a>
                 </div>
             </article>
-            <img src={astronaut} />
         </section>
     );
 }
