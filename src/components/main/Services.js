9ee41aae3844998625226cd07360d1b5d0157e10
Heloisa <jauheloisa@gmail.com>
2022-05-30 12:15:21 -0300

    serviços ok + ajustes gerais

--- src/components/main/Services.js
+++ src/components/main/Services.js
@@ -5,21 +5,25 @@ function Services() {
     return (
         <section id="services">
             <h2>Serviços</h2>
-            {/* <article className="content">
+            <article className="content">
                 <div className="item">
-                    <Icon.GitHub />
-                    <h4>UI Design</h4>
-                    <p></p>
+                    <Icon.Code color="#013C89" size={52} />
+                    <h4>Website</h4>
+                    <p>Construção de Websites institucionais e pessoais, blogs e sistemas web no geral.</p>
                 </div>
+                <div className='whiteSpace'></div>
                 <div className="item">
-                    <Icon.Instagram />
-                    <p>Desenvolvimento Web</p>
+                    <Icon.Monitor color="#013C89" size={52} />
+                    <h4>Landing Page</h4>
+                    <p>Desenvolvimento de Landing Pages para captura de público para seu negócio.</p>
                 </div>
+                <div className='whiteSpace'></div>
                 <div className="item">
-                    <Icon.Linkedin />
-                    <p>Javascript</p>
+                    <Icon.MousePointer color="#013C89" size={52} />
+                    <h4>Hotsite</h4>
+                    <p>Criação de Hotsites para a divulgação de seus produtos de forma profissional.</p>
                 </div>
-            </article> */}
+            </article>
         </section>
     );
 }
