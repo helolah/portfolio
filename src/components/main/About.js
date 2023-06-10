d478e7450cc0da6e8cc071854bc9afcbb201494b
heloisa <euheloisafs@gmail.com>
2023-06-10 17:07:47 -0300

    ajuste texto - sobre mim

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -11,7 +11,7 @@ function About() {
             <h2>Quem sou</h2>
             <article>
                 <p>
-                    Meu nome é Heloisa Fernanda, tenho {getMyAge()} anos e moro no interior de São Paulo. Sou formada no curso de Sistemas para Internet pela <a href="http://fatecjahu.edu.br/" target={'_blank'}>FATEC Jahu</a>, e atuo no ramo de programação de aplicações para Web, onde minhas atividades profissionais se focam no Front-end e Web Design.
+                    Meu nome é Heloisa Fernanda, tenho {getMyAge()} anos e moro no interior de São Paulo. Sou formada no curso de Sistemas para Internet pela <a href="http://fatecjahu.edu.br/" target={'_blank'}>FATEC Jahu</a>, e atuo no ramo de programação de aplicações para Web, onde minhas atividades profissionais se focam tanto no Front-end, quanto Back-End e Web Design.
                     <br/><br/>
                     Atualmente trabalho como programadora Web freelancer, desenvolvendo diversas plataformas para eventos online e presenciais. Além disso, tenho projetos de estudos em ReactJS que você pode verificar mais abaixo na sessão de "Projetos" ou acessando meu perfil no <a href="https://github.com/helolah" target={'_blank'}>Github!</a>
                 </p>
