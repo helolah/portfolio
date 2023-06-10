b147ba7a1468d4f0a468703a386012b2b7e10199
heloisa <euheloisafs@gmail.com>
2023-06-10 16:20:41 -0300

    add projetos na sessao de projetos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     return (
         <article>
             <div className="footer-content">
-                <p className="powered">Powered by <a href="https://github.com/helolah" target={'_blank'}>Helolah</a>.</p>
+                <p className="powered">Powered by <a href="https://github.com/helolah" target={'_blank'}>Heloisa</a>.</p>
                 <p>&copy; {getCurrentYear()} Todos os direitos reservados.</p>
             </div>
         </article>
--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -1,11 +1,28 @@
-import * as Icon from 'react-feather';
+import ataesp from "../../assets/img/ataesp.jpg";
+import galdino from "../../assets/img/galdino.jpg";
 
 function Projects() {
 
     return (
         <section id="projects">
             <h2>Projetos</h2>
-            
+            <article className="content">
+                <div className="item">
+                    <a href="http://www.ata.org.br" target='blank' title='Ir para o site'>
+                        <img src={ataesp} alt="Imagem da página inicial do site ATAESP" />
+                        <h4>ATAESP</h4>
+                        <p>Website institucional voltado para a área de transportes, com uma sessão no modelo de blog e área para login.</p>
+                    </a>
+                </div>
+                <div className='whiteSpace'></div>
+                <div className="item">
+                    <a href="https://galdinoimplantes.com.br" target='blank' title='Ir para o site'>
+                        <img src={galdino} alt="Imagem da página inicial do site Galdino" />
+                        <h4>GALDINO</h4>
+                        <p>Website institucional voltado para a área de odontologia, com uma sessão no modelo de blog.</p>
+                    </a>
+                </div>
+            </article>
         </section>
     );
 }
